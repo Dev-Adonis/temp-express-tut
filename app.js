5e47d3817456dcfe1fc5da4fdf4364bf5d2e7bd1
Dev-Adonis <146042659+Dev-Adonis@users.noreply.github.com>
2026-02-10 16:14:50 -0600

    imported middleware logger to 'use' for all routes

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
+const logger = require('./express-logger-middleware');
 
-const logger = (req, res, next) => {
-    const method = req.method;
-    const url = req.url;
-    const time = new Date().getFullYear();
-    console.log(method, url, time);
-    // res.send('Testing...');
-    next();
-}
+app.use(logger);
 
-app.get('/', logger, (req, res) => {
-    res.send("Home");
+app.get('/', (req, res) => {
+    res.send('Home');
 })
 
-app.get('/about', logger, (req, res) => {
-    res.send("About");
+app.get('/about', (req, res) => {
+    res.send('About');
 })
 
+app.get('/api/products', (req, res) => {
+    res.send('Products');   
+})
+
+app.get('/api/items', (req, res) => {
+    res.send('Items');
+})
 
 app.listen(5000, () => {
     console.log('server listening on port 5000');
