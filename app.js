8ce3d3bee2b594fa285bf0618d13146149f4d354
Dev-Adonis <146042659+Dev-Adonis@users.noreply.github.com>
2026-02-09 21:49:32 -0600

    serve from html file instead of manual res creation

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const http = require('http');
+const { readFileSync} = require('fs');
+
+const homePage = readFileSync('./index.html');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
     if(url === '/') {
         res.writeHead(200, {'content-type': 'text/html'});
-        res.write(`<h1>Home Page</h1>`);
+        res.write(homePage);
         res.end();
     } else if (url === '/about') {
         res.writeHead(200, {'content-type': 'text/html'});
