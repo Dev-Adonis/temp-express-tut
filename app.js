16a7b9a7cca6a22dc76a6b736c57c84674f6f577
Dev-Adonis <146042659+Dev-Adonis@users.noreply.github.com>
2026-02-09 21:30:58 -0600

    creating a basic 200 response

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('user hit the server');
-    res.end('Welcome to the Homepage');
-
+    // console.log('user hit the server');
+    // res.end('Welcome to the Homepage');
+    res.writeHead(200, {'content-type': 'text/html'});
+    // res.end(`<h1>Home Page</h1>`); //OR
+    res.write(`<h1>Home Page</h1>`);
+    res.end();
 })
 
 
