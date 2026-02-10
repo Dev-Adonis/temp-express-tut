6476004a755ccb8a50afbfef36af40870f1cec29
Dev-Adonis <146042659+Dev-Adonis@users.noreply.github.com>
2026-02-10 10:11:58 -0600

    added query search & limit for specific req data

--- app.js
+++ app.js
@@ -14,10 +14,44 @@ app.get('/api/products', (req, res) => {
     res.json(newProducts);
 })
 
-app.get('/api/products/1', (req, res) => {
-    const singleProduct = products.find((product) => product.id === 1)
+app.get('/api/products/:productID', (req, res) => {
+    // console.log(req);
+    // console.log(req.params)
+    const {productID} = req.params;
+
+    const singleProduct = products.find((product) => product.id === Number(productID));
+    if(!singleProduct) {
+        return res.status(404).send('Product does not exist');
+    }
     res.json(singleProduct);
 })
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    console.log(req.params);
+    res.send('hello world');
+})
+
+
+app.get('/api/v1/query', (req, res) => {
+    // console.log(req.query);
+    const {search, limit} = req.query;
+    let sortedProducts = [...products];
+
+    if(search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search);
+        })
+    }
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if(sortedProducts.length < 1) {
+        // return res.status(200).send(`No product that starts with ${req.query.search}`);
+        return res.status(200).json({ success: true, data: [] } )
+    }
+    res.status(200).json(sortedProducts);
+})
+
 
 app.listen(5000, () => {
     console.log('server listening on port 5000...');
