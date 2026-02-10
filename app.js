2fb6f2f628cf950e00d135596221e4c70bb5f66d
Dev-Adonis <146042659+Dev-Adonis@users.noreply.github.com>
2026-02-09 21:42:33 -0600

    created 200 && 400 responses for /, /about, /anything else

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    // console.log('user hit the server');
-    // res.end('Welcome to the Homepage');
-    res.writeHead(200, {'content-type': 'text/html'});
-    // res.end(`<h1>Home Page</h1>`); //OR
-    res.write(`<h1>Home Page</h1>`);
-    res.end();
+    const url = req.url;
+    if(url === '/') {
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write(`<h1>Home Page</h1>`);
+        res.end();
+    } else if (url === '/about') {
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write(`<h1>About Page</h1>`);
+        res.end();
+    } else {
+        res.writeHead(404, {'content-type': 'text/html'});
+        res.write(`<h1>Uh oh! Page not found</h1>`);
+        res.end();
+    }
 })
 
 
